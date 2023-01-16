e213a26105c80e5ca961010507175278affb040f
Elia Vacas García <eliavacasgarcia@gmail.com>
2023-01-16 09:14:57 +0100

    Add files via upload

--- services/app.service.js
+++ services/app.service.js
@@ -2,71 +2,76 @@ import axios from 'axios';
 
 // Clase servicio para operaciones CRUD con la API
 const API_URL = 'https://testsuite.foxxum.com/strapi123456/api';
-class AppService{
-    /* Operaciones CRUD => Create - Read - Update - Delete */
-    // GETS (Read)
-    // Traer todas las apps
-    getAllApps(){
-        return axios.get(`${API_URL}/apps?populate=*`)
-    }
+class AppService {
+  /* Operaciones CRUD => Create - Read - Update - Delete */
+  // GETS (Read)
+  // Traer todas las apps
+  getAllApps() {
+    return axios.get(`${API_URL}/apps?populate=*`);
+  }
 
-    // Traer una app por su id
-    getApp(id){
-        return axios.get(`${API_URL}/apps?filters[id][$eq]=${id}&populate=*`)
-    }
+  // Traer una app por su id
+  getApp(id) {
+    return axios.get(`${API_URL}/apps?filters[id][$eq]=${id}&populate=deep`);
+  }
 
-    // Traer todos los países
-    // Al haber 250 países y sólo poder traer 100 por petición debo hacer 3 peticiones
-    getCountries1(){
-        return axios.get(`${API_URL}/countries?pagination[pageSize]=100`)
-    }
-    getCountries2(){
-        return axios.get(`${API_URL}/countries?pagination[pageSize]=100&pagination[page]=2`)
-    }
-    getCountries3(){
-        return axios.get(`${API_URL}/countries?pagination[pageSize]=100&pagination[page]=3`)
-    }
+  // Traer todos los países
+  // Al haber 250 países y sólo poder traer 100 por petición debo hacer 3 peticiones
+  getCountries1() {
+    return axios.get(`${API_URL}/countries?pagination[pageSize]=100`);
+  }
 
-    // Traer todas las categorías
-    getCategories(){
-        return axios.get(`${API_URL}/app-categories`)
-    }
+  getCountries2() {
+    return axios.get(`${API_URL}/countries?pagination[pageSize]=100&pagination[page]=2`);
+  }
 
-    // Traer todos los players
-    getPlayers(){
-        return axios.get(`${API_URL}/players`)
-    }
+  getCountries3() {
+    return axios.get(`${API_URL}/countries?pagination[pageSize]=100&pagination[page]=3`);
+  }
 
-    // Traer todos los dispositivos
-    getDevices(){
-        return axios.get(`${API_URL}/devices`)
-    }
+  // Traer todas las categorías
+  getCategories() {
+    return axios.get(`${API_URL}/app-categories`);
+  }
 
-    // Traer todos los idiomas
-    // Al haber 184 idiomas y sólo poder traer 100 por petición debo hacer 2 peticiones
-    getLanguages1(){
-        return axios.get(`${API_URL}/languages?pagination[pageSize]=100`)
-    }
-    getLanguages2(){
-        return axios.get(`${API_URL}/languages?pagination[pageSize]=100&pagination[page]=2`)
-    }
+  // Traer todos los players
+  getPlayers() {
+    return axios.get(`${API_URL}/players`);
+  }
 
-    // POST (Create)
-    // Añadir nueva app
-    addNewApp(app){
-        return axios.post(`${API_URL}/apps`, {data:app})
-    }
+  // Traer todos los dispositivos
+  getDevices() {
+    return axios.get(
+      `${API_URL}/devices?fields=fxmId,id,code,portalIds,appCodeManager&populate=countries&pagination[limit]=200`
+    );
+  }
 
-    // PUT (Update)
-    // Actualizar los campos de una app existente seleccionada por su id
-    updateApp(id, app){
-        return axios.put(`${API_URL}/apps/${id}`, {data:app})
-    }
+  // Traer todos los idiomas
+  // Al haber 184 idiomas y sólo poder traer 100 por petición debo hacer 2 peticiones
+  getLanguages1() {
+    return axios.get(`${API_URL}/languages?pagination[pageSize]=100`);
+  }
 
-    // DELETE (Delete)
-    deleteApp(id){
-        return axios.delete(`${API_URL}/apps/${id}`)
-    }
+  getLanguages2() {
+    return axios.get(`${API_URL}/languages?pagination[pageSize]=100&pagination[page]=2`);
+  }
+
+  // POST (Create)
+  // Añadir nueva app
+  addNewApp(app) {
+    return axios.post(`${API_URL}/apps`, { data: app });
+  }
+
+  // PUT (Update)
+  // Actualizar los campos de una app existente seleccionada por su id
+  updateApp(id, app) {
+    return axios.put(`${API_URL}/apps/${id}`, { data: app });
+  }
+
+  // DELETE (Delete)
+  deleteApp(id) {
+    return axios.delete(`${API_URL}/apps/${id}`);
+  }
 }
 
 export default new AppService();
--- services/issue.service.js
+++ services/issue.service.js
@@ -2,28 +2,23 @@ import axios from 'axios';
 
 // Clase servicio para operaciones CRUD con la API
 const API_URL = 'https://testsuite.foxxum.com/strapi123456/api';
-class IssueService{
-    // GETS (Read)
-    // Traer todas las issues
-    getAllIssues(appId){
-        return axios.get(`${API_URL}/apps?filters[id][$eq]=${appId}&fields[0]=id&populate=issues`)
-    }
+class IssueService {
+  // GETS (Read)
+  // Método auxiliar para traer la categoría de un issue
+  getIssueCategoryAux(appId) {
+    return axios.get(`${API_URL}/apps/${appId}?populate=deep`);
+  }
 
-    // Método auxiliar para traer la categoría de un issue
-    getIssueCategoryAux(appId){
-        return axios.get(`${API_URL}/apps/${appId}?populate=deep`)
-    }
+  // Traer de la API las categorías posibles para un issue
+  getIssueCategories() {
+    return axios.get(`${API_URL}/Issue-Categories`);
+  }
 
-    // Traer de la API las categorías posibles para un issue
-    getIssueCategories(){
-        return axios.get(`${API_URL}/Issue-Categories`)
-    }
-
-    // PUT (Update)
-    // Actualizar los issues de una app existente seleccionada por su id
-    addNewIssue(id, issues){
-        return axios.put(`${API_URL}/apps/${id}`, {data:{issues:issues}})
-    }
+  // PUT (Update)
+  // Actualizar los issues de una app existente seleccionada por su id
+  addNewIssue(id, issues) {
+    return axios.put(`${API_URL}/apps/${id}`, { data: { issues } });
+  }
 }
 
 export default new IssueService();
